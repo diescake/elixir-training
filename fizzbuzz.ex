cf3c404a19ea2bd724afc1ed74916c1db663287f
diescake <daisuke.kondo@access-company.com>
2019-04-01 04:44:42 +0900

    Piped fizzbuzz

--- fizzbuzz.ex
+++ fizzbuzz.ex
@@ -4,7 +4,7 @@ defmodule FizzBuzz do
       rem(n, 15) == 0 -> "FizzBuzz"
       rem(n, 3) == 0 -> "Fizz"
       rem(n, 5) == 0 -> "Buzz"
-      true -> n
+      true -> n |> to_string
     end
   end
 
@@ -12,5 +12,20 @@ defmodule FizzBuzz do
     Enum.each(1..limit, fn i ->
       IO.puts(fizzbuzz(i))
     end)
+  end
+end
+
+defmodule PipedFizzBuzz do
+  def fizzbuzz(n) do
+    cond do
+      rem(n, 15) == 0 -> "FizzBuzz"
+      rem(n, 3) == 0 -> "Fizz"
+      rem(n, 5) == 0 -> "Buzz"
+      true -> n
+    end
+  end
+
+  def run(limit) do
+    1..limit |> Enum.each(fn i -> i |> fizzbuzz |> IO.puts() end)
   end
 end
